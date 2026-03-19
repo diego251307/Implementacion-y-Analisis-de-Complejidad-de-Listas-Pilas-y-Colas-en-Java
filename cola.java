7926919bb9a557685f4badfcdfe5573097d3523f
diego251307 <2513edb@gmail.com>
2026-03-18 23:58:30 -0500

    Update cola.java

--- cola.java
+++ cola.java
@@ -11,7 +11,7 @@ public class MyQueque<T extends Comparable<T>> {
     @SuppressWarnings("unchecked")
     public void enqueque(T x){
         if(size>=arreglo.length){
-            T[] nArreglo = (T[])new Object[arreglo.length*2]
+            T[] nArreglo = (T[])new Object[arreglo.length*2];
             for (int i=0;i<arreglo.length;i++){
                 nArreglo[i]=arreglo[i];
             }
@@ -21,19 +21,35 @@ public class MyQueque<T extends Comparable<T>> {
         size++;
     }
     // dequeque - Diego
+    public T dequeue(){
+        T dq = arreglo[0];
+        for (int i = 0; i<size, i++){
+            arreglo[i] == arreglo[i+1];
+        }
+        size--;
+        return dq;
+    }
     // front - Diego
+    public T front(){
+        if (!isEmpty()){
+            return arreglo[0];
+        }
+    }
     // isEmpty - Julian
-    public bool isEmpty(){
+    public boolean isEmpty(){
         return size<=0;
     }
     // size - Diego
+    public int size(){
+        return size;
+    }
     // delete - Julian
     public void delete(T n){
         bool aux=false;
         for (int i=0;i<arreglo.length-1;i++){
             if(n==arreglo[i]) aux=true;
             if(aux){
-                arreglo[i]=arreglo[i+1]
+                arreglo[i]=arreglo[i+1];
             }
         }
         if(aux || arreglo[arreglo.length-1]==n) size--;
